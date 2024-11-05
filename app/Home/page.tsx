41e5f6de32e74482d19866c0e92a24c64b7a3ed3
Madeline Wong <mwong20@bu.edu>
2024-11-05 01:15:55 -0500

    having problems displaying homepage and making API requests so far

---
+++ app/Home/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import styled from "styled-components";
+import { useState } from "react";
+import Link from "next/link";
+
+const StyledDiv=styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export default function Home(){
+    const [city, setCity] = useState("");
+
+    return (
+        <StyledDiv>
+            <h1>Find the weather in any city!</h1>
+            <p>Enter a city name below to get the current weather</p>
+            <input type="text" value={city} placeholder="City Name" onChange={(e) => setCity(e.target.value)} />
+            <Link href={`/${city}`}>Get Weather</Link>
+        </StyledDiv>
+    );
+
+}
---
+++ types.ts
@@ -0,0 +1,7 @@
+export type Weather = {
+    tempmin: number,
+    tempmax: number,
+    datetime: any,
+    description: string, 
+    conditions: string,
+};
